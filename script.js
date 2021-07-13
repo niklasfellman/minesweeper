6ad7ae351dc0dff03b21c5a294c395c36fdd0097
unknown <purjotti@gmail.com>
2021-07-13 12:07:25 +0300

    cleaning up

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-let numberSquares = 15
-
+let numberSquares = 20
 class square {
-    constructor(number, position, active,flagged, gotBomb) {
+    constructor(number, position, active, flagged, gotBomb) {
         this.number = number;
         this.position = position;
         this.active = active;
@@ -21,18 +20,34 @@ function placeBomb() {
 }
 
 function checkForBombs(x) {
-    let arr =[]
+    let arr = []
 
     arr.push(boardArr[x[0]][x[1]])
-    if (x[0] != 0){arr.push(boardArr[x[0]-1][x[1]])}
-    if (x[0] != numberSquares-1) {arr.push(boardArr[x[0]+1][x[1]])}
-    if (x[1] != 0){arr.push(boardArr[x[0]][x[1]-1])}
-    if (x[1] != numberSquares - 1){arr.push(boardArr[x[0]][x[1]+1])}
-    if (x[0] != 0 && x[1] != 0){arr.push(boardArr[x[0]-1][x[1]-1])}
-    if (x[0] != numberSquares -1 && x[1] != numberSquares -1){arr.push(boardArr[x[0]+1][x[1]+1])}
-    if (x[0] != 0 && x[1] != numberSquares -1) {arr.push(boardArr[x[0]-1][x[1]+1])}
-    if (x[0] != numberSquares-1 && x[1] != 0) {arr.push(boardArr[x[0]+1][x[1]-1])}
-   
+    if (x[0] != 0) {
+        arr.push(boardArr[x[0] - 1][x[1]])
+    }
+    if (x[0] != numberSquares - 1) {
+        arr.push(boardArr[x[0] + 1][x[1]])
+    }
+    if (x[1] != 0) {
+        arr.push(boardArr[x[0]][x[1] - 1])
+    }
+    if (x[1] != numberSquares - 1) {
+        arr.push(boardArr[x[0]][x[1] + 1])
+    }
+    if (x[0] != 0 && x[1] != 0) {
+        arr.push(boardArr[x[0] - 1][x[1] - 1])
+    }
+    if (x[0] != numberSquares - 1 && x[1] != numberSquares - 1) {
+        arr.push(boardArr[x[0] + 1][x[1] + 1])
+    }
+    if (x[0] != 0 && x[1] != numberSquares - 1) {
+        arr.push(boardArr[x[0] - 1][x[1] + 1])
+    }
+    if (x[0] != numberSquares - 1 && x[1] != 0) {
+        arr.push(boardArr[x[0] + 1][x[1] - 1])
+    }
+
     let bombs = []
 
     for (x of arr) {
@@ -44,16 +59,16 @@ function checkForBombs(x) {
 }
 
 
-function checkWin (arr){
+function checkWin(arr) {
 
-    for (let i = 0;i<arr.length;i++){
-        for (let j = 0;j<arr.length;j++){
-        if (arr[i][j].active == true){
-        return false
-    }
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[i][j].active == true) {
+                return false
+            }
         }
     }
-return true
+    return true
 }
 
 let boardArr = []
@@ -68,17 +83,14 @@ for (let i = 0; i < numberSquares; i++) {
     boardArr.push([])
 
     for (let j = 0; j < numberSquares; j++) {
-        boardArr[i].push(new square(number, `${i}${j}`, false,false, placeBomb()))
+        boardArr[i].push(new square(number, `${i}${j}`, false, false, placeBomb()))
         let newDomSqr = document.createElement("div")
-
         newDomSqr.className = "square"
-
         //shows where bombs are, remove later
         if (boardArr[i][j].gotBomb == true) {
             boardArr[i][j].active = true
-          //newDomSqr.classList.add("active")
+            //newDomSqr.classList.add("active")
         }
-
         newDomSqr.setAttribute("data-position", `${i}-${j}`)
         board.appendChild(newDomSqr)
         number++
@@ -87,7 +99,7 @@ for (let i = 0; i < numberSquares; i++) {
 
 
 
-function lostGame(x){
+function lostGame(x) {
 
     let lostP = document.createElement("p")
     lostP.id = "lost"
@@ -96,11 +108,14 @@ function lostGame(x){
 
     let playAgain = document.querySelector("#lost")
 
-playAgain.addEventListener("click", x => {
-    location.reload()
+    playAgain.addEventListener("click", x => {
+        location.reload()
     })
 
 }
+
+
+
 
 let squares = document.querySelectorAll(".square")
 console.log(squares[0])
@@ -111,9 +126,19 @@ board.addEventListener("click", x => {
         console.log("lost")
         lostGame("You stepped on a mine - Click to play again");
     }
+
+
     arr = checkForBombs(pos)
+
+
+    if (arr.length === 0) {
+        console.log("zero")
+    }
     x.target.classList.add("adjecent")
     x.target.innerText = arr.length
+
+
+
 })
 
 
@@ -121,12 +146,14 @@ board.addEventListener("contextmenu", x => {
     x.preventDefault()
     let pos = x.target.dataset.position.split("-").map(x => parseInt(x))
     boardArr[pos[0]][pos[1]].active = !boardArr[pos[0]][pos[1]].active
-    
+
     x.target.classList.toggle("flagged")
 
-   // x.target.innerText = "🚩"
+    // x.target.innerText = "🚩"
 
-    if (checkWin(boardArr)){lostGame("You found all the mines - Click to play again")}
-  console.log(checkWin(boardArr)) 
+    if (checkWin(boardArr)) {
+        lostGame("You found all the mines - Click to play again")
+    }
+    console.log(checkWin(boardArr))
 
 })
